0e8fa824038e9fe6bf918f3204c526cdade0b5d4
niels <>
2012-03-14 19:16:02 +0100

    formating src

--- src/filesplit/FileRead.java
+++ src/filesplit/FileRead.java
@@ -9,31 +9,28 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
-
 /**
  *
  * @author Niels
  */
 public class FileRead {
-    
+
     static String line;
     static ArrayList<String> array = new ArrayList<>();
-    
-    public static ArrayList<String> readfile(String filename){
-        
-        try{
-			FileReader fread = new FileReader(filename); //öffntet die gewünschte txt Datei
-			BufferedReader in = new BufferedReader(fread); // txt wird in den BufferReader geladen
-			
-			for(int i = 0;(line = in.readLine())!=null; i++){ // durchlaufen, bis line = null(datei zu ende ist)
-				array.add(line); // array[i] wird mit line(der aktuellen Zeile) belegt	
-                               
-			}
-		}
-		catch(IOException e){
-			System.out.println("IO-Fehler!");
-		}
+
+    public static ArrayList<String> readfile(String filename) {
+
+        try {
+            FileReader fread = new FileReader(filename); //öffntet die gewünschte txt Datei
+            BufferedReader in = new BufferedReader(fread); // txt wird in den BufferReader geladen
+
+            for (int i = 0; (line = in.readLine()) != null; i++) { // durchlaufen, bis line = null(datei zu ende ist)
+                array.add(line); // array[i] wird mit line(der aktuellen Zeile) belegt	
+
+            }
+        } catch (IOException e) {
+            System.out.println("IO-Fehler!");
+        }
         return array;
     }
-    
 }
